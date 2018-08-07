d17edc3b55f9f54657cc668be669fc956c17685f
Nick Rowlands <nick@rowlando.com>
2018-08-07 15:01:53 +0100

    Tests are configurable with environment variables

--- tests/call-centre/protractor.conf.js
+++ tests/call-centre/protractor.conf.js
@@ -1,15 +1,26 @@
 var puppeteer = require("puppeteer");
+var chromedriver = require("chromedriver");
+var chromedriverPath = chromedriver.path;
+
+var baseUrl = process.env.CLA_FRONTEND_URL || "http://localhost:8001/";
+var chromeOptions = {
+  args: ["--no-sandbox"]
+};
+
+if (process.env.HEADLESS) {
+  chromeOptions.binary = puppeteer.executablePath();
+  chromeOptions.args.push("--headless", "--disable-dev-shm-usage");
+}
 
 exports.config = {
+  directConnect: true,
+  chromeDriver: chromedriverPath,
   capabilities: {
     browserName: "chrome",
-    chromeOptions: {
-      args: ["--headless", "--no-sandbox", "--disable-dev-shm-usage"],
-      binary: puppeteer.executablePath()
-    }
+    chromeOptions: chromeOptions,
+    acceptSslCerts: true
   },
-  baseUrl: "http://cla_frontend/",
-  directConnect: true,
+  baseUrl: baseUrl,
   framework: "jasmine",
   jasmineNodeOpts: {
     showColors: true // Use colors in the command line report.
