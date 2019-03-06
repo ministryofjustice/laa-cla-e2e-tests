e2ddd07a91e086a184fbab26aad68740b84fb5d4
Katy600 <katherine.mccann@digital.justice.gov.uk>
2019-03-06 17:24:13 +0000

    Comment out SCope diagnosis function inside Face-to-face test

--- tests/check-legal-aid/test/specs/face-to-face-page.js
+++ tests/check-legal-aid/test/specs/face-to-face-page.js
@@ -1,16 +1,28 @@
 "use strict";
 
 var constants = require("../modules/constants");
+var log = require("../modules/log");
+
+this.perform(function() {
+log.command('HELLO FROM THE FACE TO FACE PAGE...');
 
 module.exports = {
+  before : function(browser) {
+    console.log('About to click on START')
+  },
+
+  after : function(browser) {
+    console.log('Closing down...');
+  },
+
   "Start page": function(client) {
     client.startService();
   },
 
-  "@disabled": true,
-  "Scope diagnosis": function(client) {
-    client.scopeDiagnosis(constants.SCOPE_PATHS.clinnegFaceToFace);
-  },
+//  "@disabled": false,
+//  "Scope diagnosis": function(client) {
+//    client.scopeDiagnosis(constants.SCOPE_PATHS.clinnegFaceToFace);
+//  },
 
   "Face-to-face page": function(client) {
     client
@@ -25,12 +37,13 @@ module.exports = {
   },
 
   "Find legal adviser search": function(client) {
+    log.command("HELLO");
     client
       .setValue('input[name="postcode"]', "w22dd", function() {
         console.log("     • Enter postcode `w22dd`");
       })
       .conditionalFormSubmit(true)
-      .assert.urlContains("/scope/refer/legal-adviser", "    - Page is ready")
+      .assert.urlContains("/scope/refer/legal-adviser", "    - Page is NOT ready")
       .waitForElementVisible(
         ".search-results-container",
         5000,
